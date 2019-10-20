84bbf93a233721d13cddc7604288bb8bf3b30198
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-10-20 14:44:31 +0800

    added fields, constructor, setter getter methods

--- src/main/java/com/thoughtworks/parking_lot/order/Order.java
+++ src/main/java/com/thoughtworks/parking_lot/order/Order.java
@@ -1,9 +1,48 @@
 package com.thoughtworks.parking_lot.order;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Order {
-    
+    @Id
+    private Long id;
+    private String parkingLotName;
+    private String plateNumber;
+    private String status;
 
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getParkingLotName() {
+        return parkingLotName;
+    }
+
+    public void setParkingLotName(String parkingLotName) {
+        this.parkingLotName = parkingLotName;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/thoughtworks/parking_lot/parkingLot/ParkingLot.java
+++ src/main/java/com/thoughtworks/parking_lot/parkingLot/ParkingLot.java
@@ -1,4 +1,39 @@
 package com.thoughtworks.parking_lot.parkingLot;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ParkingLot {
+    @Id
+    private String name;
+    private Integer capacity;
+    private String location;
+
+    public ParkingLot() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
