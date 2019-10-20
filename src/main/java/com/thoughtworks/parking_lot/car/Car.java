d8785e423852e24c996d68c013d517f47f7b4385
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-10-20 20:06:33 +0800

    updated Story2 AC1

--- src/main/java/com/thoughtworks/parking_lot/car/Car.java
+++ src/main/java/com/thoughtworks/parking_lot/car/Car.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.parking_lot.car;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+    @Id
     private String plateNumber;
 
     public String getPlateNumber() {
