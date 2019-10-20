bdbc4eb295018caf1bee0b3067468de63c1126a8
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-10-20 14:22:29 +0800

    updated build.gradle to include h2

--- src/main/java/com/thoughtworks/parking_lot/order/Order.java
+++ src/main/java/com/thoughtworks/parking_lot/order/Order.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.parking_lot.order;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Order {
+
 }
--- src/main/java/com/thoughtworks/parking_lot/order/OrderRepository.java
+++ src/main/java/com/thoughtworks/parking_lot/order/OrderRepository.java
@@ -1,4 +1,6 @@
 package com.thoughtworks.parking_lot.order;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
 }
